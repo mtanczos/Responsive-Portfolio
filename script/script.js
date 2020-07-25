800bcee22d81371fec025fdf442a198d7632d2fe
Mate Tanczos <mtanczos79@gmail.com>
2020-07-25 14:05:03 -0400

    add comments to pages

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-//this function 
+//this function pops out the clicked on image
 $(function() {
     $('.pop').on('click', function() {
       $('.imagepreview').attr('src', $(this).find('img').attr('src'));
